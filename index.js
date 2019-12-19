187d6ee4385081b72a6eab201b3321266f55d9a4
Josue Rodriguez <smexyjosue@gmail.com>
2019-12-19 14:53:22 -0800

    need 1 more

--- index.js
+++ index.js
@@ -41,7 +41,23 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    (this.stomach = []), (this.name = name), (this.age = age);
+  }
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      return this.stomach.push(someFood);
+    }
+  }
 
+  poop() {
+    if (this.stomach.length === 10) {
+      return (this.stomach = []);
+    }
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +75,27 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    (this.model = model),
+      (this.milesPerGallon = milesPerGallon),
+      (this.tank = 0),
+      (this.odometer = 0),
+      (max = 200);
+  }
+  fill(gallons) {
+    if (this.tank === 0) {
+      return (this.tank = this.tank + gallons);
+    }
+  }
+  drive(distance) {
+    if (distance < max) {
+      this.odometer = this.odometer + distance;
+      return (this.tank = (this.tank - distance) / this.milesPerGallon);
+    } else {
+      this.odometer = this.odometer + max;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
@@ -75,7 +111,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(object) {
+    (this.name = object.name),
+      (this.age = object.age),
+      (this.location = object.location);
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -92,8 +135,20 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(attributes) {
+    super(attributes);
+    (this.specialty = attributes.specialty),
+      (this.favLanguage = attributes.favLanguage),
+      (this.catchPhrase = attributes.catchPhrase);
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
 
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -111,8 +166,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(item) {
+    super(item);
+    this.previousBackground = item.previousBackground;
+    this.className = item.className;
+    this.favSubjects = item.favSubjects;
+  }
+  listSubjects() {
+    return `Hi, I like ${this.favSubjects}`;
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
 
 /*
@@ -128,8 +197,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(box) {
+    super(box);
+    (this.gradClassName = box.gradClassName),
+      (this.favInstructor = box.favInstructor);
+  }
+  standUp(slack) {
+    return `${this.name} announces to ${slack}, @channel standy times!`;
+  }
+  debugsCode(object, subject) {
+    return `${this.name} debugs ${object.name}'s code on ${subject}`;
+  }
 }
 
 /*
@@ -144,13 +223,27 @@ class ProjectManager {
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Lambdasian) { module.exports.Lambdasian = Lambdasian }
-  if (Instructor) { module.exports.Instructor = Instructor }
-  if (Student) { module.exports.Student = Student }
-  if (ProjectManager) { module.exports.ProjectManager = ProjectManager }
+if (typeof exports !== "undefined") {
+  module.exports = module.exports || {};
+  if (Airplane) {
+    module.exports.Airplane = Airplane;
+  }
+  if (Person) {
+    module.exports.Person = Person;
+  }
+  if (Car) {
+    module.exports.Car = Car;
+  }
+  if (Lambdasian) {
+    module.exports.Lambdasian = Lambdasian;
+  }
+  if (Instructor) {
+    module.exports.Instructor = Instructor;
+  }
+  if (Student) {
+    module.exports.Student = Student;
+  }
+  if (ProjectManager) {
+    module.exports.ProjectManager = ProjectManager;
+  }
 }
